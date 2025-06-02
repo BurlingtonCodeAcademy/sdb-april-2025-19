db92a320a7cae23788c9b8d679c8b9dabf492c85
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-02 17:42:13 -0500

    by tag name

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const myHtml = document.querySelector('html')
+let myH1 = document.getElementsByTagName('h1')
 
 console.log(myHtml);
+
+console.log(myH1)
+console.log("innerHTML: ", myH1[0].innerHTML)
+console.log("textContent: ", myH1[0].textContent)
+console.log("innerText: ", myH1[0].innerText)
