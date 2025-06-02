6aefb585fe8a5aaf1920c04a616e572019fe02c3
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-02 18:12:02 -0500

    dom traversal

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const myHtml = document.querySelector('html')
 let myH1 = document.getElementsByTagName('h1')
+const topSection = document.getElementsByClassName('top-section')
+const navbar = document.getElementById('navbar');
+const otherNavbar = document.querySelector(".navbar")
 
 console.log(myHtml);
 
@@ -7,3 +10,8 @@ console.log(myH1)
 console.log("innerHTML: ", myH1[0].innerHTML)
 console.log("textContent: ", myH1[0].textContent)
 console.log("innerText: ", myH1[0].innerText)
+
+console.log("top section: ", topSection);
+
+console.log("navbar: ", navbar);
+console.log("otherNavbar: ", otherNavbar);
