9d8909fa39d9c4567988c32741794755c35101e9
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-02 19:04:58 -0500

    some stuff

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let myH1 = document.getElementsByTagName('h1')
 const topSection = document.getElementsByClassName('top-section')
 const navbar = document.getElementById('navbar');
 const otherNavbar = document.querySelector(".navbar")
+let myDiv = document.getElementById('my-div');
+let myParagraph = document.createElement('p')
 
 console.log(myHtml);
 
@@ -15,3 +17,15 @@ console.log("top section: ", topSection);
 
 console.log("navbar: ", navbar);
 console.log("otherNavbar: ", otherNavbar);
+
+myH1[0].innerHTML = "Chris has short feet";
+
+myH1[1].innerHTML = "spiderman is fun"
+
+console.log("myDiv: ", myDiv);
+
+myParagraph.textContent = "This is a new paragraph created by JavaScript";
+navbar.prepend(myParagraph);
+myDiv.appendChild(myH1[0]);
+myDiv.appendChild(myH1[1]);
+myDiv.appendChild(myH1[2]);
